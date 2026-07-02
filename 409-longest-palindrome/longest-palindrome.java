654dcfd144bb02bf93f00a236124371a6bd3d14c
Shekhar Hotkar <shekharhotkar2002@gmail.com>
2026-07-03 01:52:09 +0530

    Time: 7 ms (49.25%) | Memory: 43.2 MB (41.30%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestPalindrome(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int length = 0;
+        boolean hasOdd = false;
+
+        for (int count : map.values()) {
+
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) {
+            length++;
+        }
+
+        return length;
+    }
+}
